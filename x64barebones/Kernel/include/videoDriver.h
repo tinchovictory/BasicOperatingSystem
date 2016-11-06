e153330f6af510db2edf1f5cea5e9dda514f35bc
Martin <martin@victory.com.ar>
2016-11-06 17:34:21 -0300

    videoDriver.h

---
+++ x64barebones/Kernel/include/videoDriver.h
@@ -0,0 +1,15 @@
+#ifndef _VIDEO_DRIVER_H_
+#define _VIDEO_DRIVER_H_
+
+#include <stdint.h>
+
+void clearScreen();
+
+void print( const char * string );
+
+void newLine();
+
+void backSpace();
+
+
+#endif
