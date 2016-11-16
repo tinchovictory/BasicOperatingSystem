a5418bc068c8e7371cd92fcd9fc366a0fd34173b
Your Name <you@example.com>
2016-11-16 19:23:59 -0300

    ESTA DICIENDO HOLA!

--- x64barebones/Kernel/RTL8139.c
+++ x64barebones/Kernel/RTL8139.c
@@ -6,6 +6,18 @@
 #define IO_ADDRESS 0xC000
 #define TURN_ON 0x52
 
+#define RECIEVE_DESC 0x30
+#define TSAD0 0x20
+#define TSAD1 0x24
+#define TSAD2 0x28
+#define TSAD3 0x2C
+
+#define RECIEVE_STATUS 0
+
+#define BUFFER_SIZE 100 
+
+static char buffer[4][BUFFER_SIZE] = {0};
+
 void turnOnRTL(){
 	sysOutByte(IO_ADDRESS + TURN_ON, 0x0);
 
@@ -13,8 +25,12 @@ void turnOnRTL(){
 	while( (sysInLong(IO_ADDRESS + 0x37) & 0x10) != 0 );
 }
 
-void initBufferRTL(char* buffer){
-	sysOutLong(IO_ADDRESS + 0x30, (uintptr_t)buffer);
+void initBufferRTL(){
+	sysOutLong(IO_ADDRESS + 0x30, (uintptr_t)buffer[0]);
+	sysOutLong(IO_ADDRESS + TSAD0, (uintptr_t)buffer[1]);
+	sysOutLong(IO_ADDRESS + TSAD1, (uintptr_t)buffer[2]);
+	sysOutLong(IO_ADDRESS + TSAD2, (uintptr_t)buffer[3]);
+	sysOutLong(IO_ADDRESS + TSAD3, (uintptr_t)buffer[4]);
 }
 
 void setImrIsr(){
@@ -29,14 +45,19 @@ void enableRTL(){
 	sysOutByte( IO_ADDRESS + 0x37, 0x0C);
 }
 
-void initRTL(){
-	char buffer[100] = {0};
+void sendmessage(){
+	buffer[1][0]='h';
+	buffer[1][1]='o';
+	buffer[1][2]=0;
+	sysOutLong( IO_ADDRESS + 0x10, 3);
 
+}
+
+void initRTL(){
 	turnOnRTL();
-	initBufferRTL(buffer);
+	initBufferRTL();
 	setImrIsr();
 	configRCR();
 	enableRTL();
-
-	ncPrint(buffer);
+	sendmessage();
 }
