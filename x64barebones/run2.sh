a77991dd99cd06ac8f0ab49e42ff0bf6c2429460
Martin <martin@victory.com.ar>
2016-11-22 23:45:03 -0300

    Run.sh

--- x64barebones/run2.sh
+++ x64barebones/run2.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -no-kvm-irqchip -device rtl8139,netdev=mynet0,mac=DE:AD:C0:FF:EE:11 -netdev socket,id=mynet0,connect=:1234
-#qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -no-kvm-irqchip -device rtl8139,mac=DE:AD:C0:FF:EE:11 -netdev socket,id=mynet0,mcast=DE.AD.C0.FF.EE.11:1234
-#qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -no-kvm-irqchip -net nic,model=rtl8139,vlan=1,macaddr=52:54:00:12:34:56 -net socket,mcast=230.0.0.1:1234
